196f82f2384021fa44cb8604d26ed8299fe2eb96
wendy nguyen <uyennhp134@gmail.com>
2026-02-05 10:57:12 -0700

    styled Link component

--- app/page.js
+++ app/page.js
@@ -5,14 +5,14 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <ul className="space-x-4">
-        <li>
+      <ul className="space-x-4 my-4">
+        <li className="inline-block border px-2 py-1 rounded bg-purple-200 shadow-md">
           <Link href="/week-2">Go to Week 2</Link>
         </li>
-        <li>
+        <li className="inline-block border px-2 py-1 rounded bg-purple-200 shadow-md">
           <Link href="/week-3">Go to Week 3</Link>
         </li>
-        <li>
+        <li className="inline-block border px-2 py-1 rounded bg-purple-200  shadow-md">
           <Link href="/week-4">Go to Week 4</Link>
         </li>
       </ul>
