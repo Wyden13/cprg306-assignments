d5e4a1da11aa004833d536562d26dcf74ed20719
wendy nguyen <uyennhp134@gmail.com>
2026-03-20 16:50:35 -0600

    modifed: firebase config

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -15,5 +15,14 @@ const firebaseConfig = {
 };
  
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+let app;
+let auth;
+
+if (firebaseConfig.apiKey) {
+  app = initializeApp(firebaseConfig);
+  auth = getAuth(app);
+}
+
+export { auth };
