06973b0e574c463b48bb6b52224c88a5aa87f2e1
wendy nguyen <uyennhp134@gmail.com>
2026-03-20 16:41:13 -0600

    modified: changed login/signup background color

--- app/week-9/login/page.js
+++ app/week-9/login/page.js
@@ -2,9 +2,9 @@ import LoginForm from "@/app/components/auth/LoginForm";
 import Link from "next/link";
     
 export default function Page() {
-    
+
     return(
-        <div className="min-h-screen flex items-center justify-center w-full bg-gray-100">
+        <div className="min-h-screen flex items-center justify-center w-full bg-gradient-to-br from-gray-900 via-gray-800 to-black">
             <LoginForm/>
         </div>
     )
--- app/week-9/register/page.js
+++ app/week-9/register/page.js
@@ -2,7 +2,7 @@ import SignUpForm from "@/app/components/auth/SignUpForm";
 
 export default function Page() {
     return(
-        <div className="min-h-screen flex items-center justify-center w-full bg-gray-100">
+        <div className="min-h-screen flex items-center justify-center w-full bg-gradient-to-br from-gray-900 via-gray-800 to-black">
             <SignUpForm/>
         </div>
     )
