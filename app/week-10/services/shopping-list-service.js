6e7846ed4a812c4bbd23a2967d5984b6e2168d51
wendy nguyen <uyennhp134@gmail.com>
2026-03-27 13:45:11 -0600

    fix firestore path

--- app/week-10/services/shopping-list-service.js
+++ app/week-10/services/shopping-list-service.js
@@ -2,14 +2,13 @@ import { db } from "@/app/firebase/config";
 import { collection, getDocs, addDoc, query, where } from "firebase/firestore";
 
 export async function getItems(userId) {
-  const itemsCollection = collection(db, "items");
-  const q = query(itemsCollection, where("userId", "==", userId));
-  return getDocs(q);
+  const itemsCollection = collection(db, "users", userId, "items");
+  return getDocs(itemsCollection);
 }
 
 // return the added item with its ID
 export async function addItem(userId, item) {
-  const itemsCollection = collection(db, "items");
-  const docRef = await addDoc(itemsCollection, { ...item, userId });
-  return { id: docRef.id, ...item, userId };
+  const itemsCollection = collection(db, "users", userId, "items");
+  const docRef = await addDoc(itemsCollection, item);
+  return { id: docRef.id, ...item };
 }
